c622edd69ff4b2144fcc6673d5b064ed47cf4991
mdnlss <rob73hall@gmail.com>
2024-03-01 13:24:06 -0500

    fix: flake.nix dead nixos-unstable url
    nix develop and nix flake update were dead as a result of a bad input

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A devShell example";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url  = "github:numtide/flake-utils";
   };
