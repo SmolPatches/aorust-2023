f149d79f85c676f1ee6375e66af7d2d211256736
mdnlss <rob73hall@gmail.com>
2024-03-05 00:42:54 -0500

    feat: filter through games yielding only valids
    matches each line using regex to find color and number
    compares that to a predefined max for each color
    prints the resulting finals
    TODO: parse valid game ids and sum them

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,16 @@
 use regex::Regex;
-
 // cube conudrum
+struct RGB {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+const max_rgb: RGB = RGB { // max rgb value acceptable
+   r:  12,
+   g:  13,
+   b:  14,
+};
+const dbg:bool = false; // set true to print additional logs
 fn unnamed_func(input:&str) {
     let mut counter = 0;
     let game_pattern = Regex::new(r"^Game\s+\d+:(\s+\d+\s+(red|green|blue),?);?").unwrap();
@@ -10,14 +20,30 @@ fn unnamed_func(input:&str) {
                                                                                    // seperator
                                                                                    // in captures
     let validate_line = |line:&str| game_pattern.is_match(line);
-    //let valid_lines: Vec<&str> = 
-    input.split("\n").into_iter().filter( |line| validate_line(line)).for_each(|line| -> () {
+    // iterate over valid lines
+    input.split("\n").into_iter().filter( |line| validate_line(line)).filter(|&line| {
         counter += 1;
-        println!("Line {}: {:?}",counter,line);
+        if dbg {
+            println!("Line {}: {:?}",counter,line);
+        }
         //for x in color_pattern.find_iter(line).map(|m| m.as_str()) {
         for (cap, [_,num,color]) in color_pattern.captures_iter(line).map(|m| m.extract()) {
-            println!("Full Capture: {}\tNum:{}\tColor:{}",cap,num,color);
+            let num_u8:u8 = num.parse().expect("num wasnt u8");
+            if dbg {
+                println!("Full Capture: {}\tNum:{}\tColor:{}",cap,num,color);
+            }
+            match color.to_ascii_lowercase().as_str() {
+                "red" if num_u8 > max_rgb.r => return false,
+                "blue" if num_u8 > max_rgb.b => return false,
+                "green" if num_u8 > max_rgb.g => return false,
+                "green" | "red" | "blue" => true,
+                _ => return false,
+            };
         }
+        true
+    }).for_each(|line| {
+        println!("Valid: {}",line);
+        // add up game ids
     });
 }
 fn main() {
